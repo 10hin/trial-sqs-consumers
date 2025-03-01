88aa779c276832ebda3dc1bf30a23d6dd92ca6e9
10hin <10hin@outlook.jp>
2025-03-01 18:46:06 +0900

    add generated code to sqsjms/

---
+++ sqsjms/src/main/java/in/_h/java/trial/sqs/consumers/sqsjms/SqsjmsApplication.java
@@ -0,0 +1,13 @@
+package in._h.java.trial.sqs.consumers.sqsjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqsjmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqsjmsApplication.class, args);
+	}
+
+}
---
+++ sqsjms/src/test/java/in/_h/java/trial/sqs/consumers/sqsjms/SqsjmsApplicationTests.java
@@ -0,0 +1,13 @@
+package in._h.java.trial.sqs.consumers.sqsjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqsjmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
