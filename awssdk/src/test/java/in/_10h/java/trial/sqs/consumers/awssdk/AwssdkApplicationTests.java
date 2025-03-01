21ceb2d98b45014ef1c5558b34c1f18bf1381113
10hin <10hin@outlook.jp>
2025-03-01 16:42:42 +0900

    add generated project source

---
+++ awssdk/src/test/java/in/_10h/java/trial/sqs/consumers/awssdk/AwssdkApplicationTests.java
@@ -0,0 +1,13 @@
+package in._10h.java.trial.sqs.consumers.awssdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwssdkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
